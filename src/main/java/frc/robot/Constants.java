8d48e8f6d946b65dab5b8d0c4725e379c67c1a7c
SvenXD <Romanhernandezsosa1@gmail.com>
2024-07-03 16:33:13 -0600

    Mech adv swerve code imported, module inverting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,176 +1,44 @@
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
 package frc.robot;
 
 import com.ctre.phoenix6.signals.InvertedValue;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import frc.robot.Subsystems.Swerve.TunerConstants;
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static final Mode currentMode = Mode.REAL;
 
-public class Constants {
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
 
+    /** Running a physics simulator. */
+    SIM,
 
-    public static class DriveConstants{
+    /** Replaying from a log file. */
+    REPLAY
 
-    public static final double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps;
-    public static final double MaxAngularRate = 1.5 * Math.PI;
-
-    public static final double traslationP = 5.0,
-                               traslationD = 0.0,
-                               rotationP = 5.0, 
-                               rotationD = 0.0;
-
-    public static final String CANBU_STRING = "Swerve_Canivore";
-    public static final int PIGEON_ID = 15;
-    
-    public static final double kTrackWidth = 0.6096;
-
-    public static final double kWheelBase = 0.635;
-  
-    public static final SwerveDriveKinematics kSwerveKinematics = new SwerveDriveKinematics(
-      new Translation2d(kWheelBase / 2, kTrackWidth / 2),
-      new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
-      new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
-      new Translation2d(-kWheelBase / 2, kTrackWidth / 2 ));
-    }
-
-  public static class Module{
-
-   public record ModuleConfig(
-      int driveID,
-      int turnID,
-      int absoluteEncoderChannel,
-      Rotation2d absoluteEncoderOffset,
-      boolean turnMotorInverted) {}
-
-        public static double mkP = 0.0,
-                             mkI = 0.0,
-                             mkD = 0.0,
-                             mkFF = 0.0,
-                             mkS = 0.0,
-                             mkV = 0.0,
-                             mkA = 0.0,
-                             mkG = 0.0,
-                             tkP = 0.0,
-                             tkD = 0.0;
-
-     public static final ModuleConfig[] moduleConfigs =
-                                   new ModuleConfig[] {
-                                     new ModuleConfig(1, 2, 3, new Rotation2d(0.343017578125), true),
-                                     new ModuleConfig(4, 5, 6, new Rotation2d(-0.039306640625), true),
-                                     new ModuleConfig(10, 11, 12, new Rotation2d(-0.283203125), true),
-                                     new ModuleConfig(7, 8, 9, new Rotation2d(-0.167236328125), true)
-                                   };
   }
 
-    public static class Shooter{
-        /* IDs */
-
-        public static final int UPPER_SHOOTER_ID = 13;
-        public static final int LOWER_SHOOTER_ID = 14;
-
-        /* SetPoints and Threesholds */
-
-        public static final int SHOOTER_THREESHOLD = 300;
-        
-        public static final int UPPER_SHOOTER_IDLE_RPM = 0;
-        public static final int LOWER_SHOOTER_IDLE_RPM = 0;
-
-        public static final int UPPER_SHOOTER_AMP_RPM = 2000;
-        public static final int LOWER_SHOOTER_AMP_RPM = 500;
-
-        public static final int UPPER_SHOOTER_FEEDER_OVER_RPM = 4500;
-        public static final int LOWER_SHOOTER_FEEDER_OVER_RPM = 4500;
-
-        public static final int UPPER_SHOOTER_FEEDER_UNDER_RPM = 6000;
-        public static final int LOWER_SHOOTER_FEEDER_UNDER_RPM = 6000;
-
-        public static final int UPPER_SHOOTER_SPEAKER_RPM = 3000;
-        public static final int LOWER_SHOOTER_SPEAKER_RPM = 3000;
-
-        /*Physical Measurements */
-
-        public static InvertedValue UPPER_SHOOTER_INVERSION = InvertedValue.Clockwise_Positive;
-        public static InvertedValue LOWER_SHOOTER_INVERSION = InvertedValue.Clockwise_Positive;
-
-        /* PID */
-
-        public static double
-        ukP = 1.4,     
-        ukI = 0,        
-        ukD = 0.025,    
-        ukS = 0.50189,
-        ukV = 0.0016926,
-        ukA = 0.00059492,    
-        lkP = 1.4,     
-        lkI = 0,        
-        lkD = 0.025,    
-        lkS = 0.50189,
-        lkV = 0.0016926,
-        lkA = 0.00059492;
-
-    }
-
-      public static class Intake{
-       /*ID */
-        public static final int INTAKE_ID = 12;
-        public static final int INTAKE_SENSOR_ID = 0;
-
-       /*Physical Measurements */
-        public static final boolean INTAKE_INVERSION = false;
-
-    }
-
-      public static class Arm{
-        /*ID */
-
-        public static final byte LEFT_ARM_ID = 9;
-        public static final byte RIGHT_ARM_ID = 10;  
-    
-        public static final int ABSOLUTE_ENCODER_ID = 17;
-    
-        public static final double ARM_GEARBOX = 320.0 / 1.0;
-    
-        /* PID */
-        public static double kP = 0.32,
-                             kI = 0.42,
-                             kD = 0.0055,
-                             kFF = 0.0,
-                             kMaxVelocityRadPerSecond = 1000,
-                             kMaxAccelerationMetersPerSecondSquared = 1000,
-                             kS = 0.013804,
-                             kV = 0.00028699,
-                             kA = 0.00052411,
-                             kG = 0.93532,
-                             kPeriod = 0.02;
-                             
-        /* SetPoints and Threesholds */
-        public static double INTAKING_POSITION = 180.5;
-
-        public static double SPEAKER_SCORING_POSITION = 160.0;
-
-        public static double INIT_POSITION = 95.0;
-        
-        public static double IDLE_UNDER_STAGE = 170.0;
-      }
-
-
-      public static final class VisionConstants {
-
-        public static final String neuralLimelight = "limelight-neural";
-        public static final String tagLimelightName = "limelight-tags";
-    
-        public static final int main_Pipeline = 0,
-                                far_Pipeline = 1,
-                                close_Pipeline = 2;
-      }
-
-  /*   _________                    
-    /   _____/__  __ ____   ____  
-    \_____  \\  \/ // __ \ /    \ 
-   /_______  /\   /\  ___/|   |  \
-           \/  \_/  \___  >___|  /
-                        \/     \/
-           */
+  public class Shooter{
+            public static InvertedValue test = InvertedValue.CounterClockwise_Positive;
   }
-
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,61 +1,131 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 
 package frc.robot;
 
-import static frc.robot.Constants.Arm.IDLE_UNDER_STAGE;
-
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.Util.LoggedDashboardChooser;
-import frc.robot.Constants.DriveConstants;
-import frc.robot.Subsystems.Swerve.Drive;
-import frc.robot.Subsystems.Swerve.GyroIOPigeon2;
-import frc.robot.Subsystems.Swerve.ModuleIO;
-import frc.robot.Subsystems.Swerve.ModuleIOKraken;
-import frc.robot.Subsystems.Swerve.Module;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.commands.DriveCommands;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIOPigeon2;
+import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.drive.ModuleIOTalonFX;
+import frc.robot.subsystems.flywheel.Flywheel;
+import frc.robot.subsystems.flywheel.FlywheelIO;
+import frc.robot.subsystems.flywheel.FlywheelIOSim;
+import frc.robot.subsystems.flywheel.FlywheelIOSparkMax;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
 
-
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
 public class RobotContainer {
+  // Subsystems
+  private final Drive drive;
+  //private final Flywheel flywheel;
 
-  private Drive drive;
-    private final CommandXboxController chassisDriver = new CommandXboxController(0);
-    private final CommandXboxController subsystemsDriver = new CommandXboxController(1);
+  // Controller
+  private final CommandXboxController controller = new CommandXboxController(0);
 
+  // Dashboard inputs
+  private final LoggedDashboardNumber flywheelSpeedInput =
+      new LoggedDashboardNumber("Flywheel Speed", 1500.0);
 
-
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    switch (Constants.currentMode) {
+      case REAL:
+        // Real robot, instantiate hardware IO implementations
+        drive =
+            new Drive(
+                new GyroIOPigeon2(true),
+                new ModuleIOTalonFX(0),
+                new ModuleIOTalonFX(1),
+                new ModuleIOTalonFX(2),
+                new ModuleIOTalonFX(3));
+      
+        break;
 
-     drive =
-              new Drive(
-                  new ModuleIOKraken(DriveConstants.moduleConfigs[0]),
-                  new ModuleIOKraken(DriveConstants.moduleConfigs[1]),
-                  new ModuleIOKraken(DriveConstants.moduleConfigs[2]),
-                  new ModuleIOKraken(DriveConstants.moduleConfigs[3]));
+      case SIM:
+        // Sim robot, instantiate physics sim IO implementations
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim());
+      //  flywheel = new Flywheel(new FlywheelIOSim());
+        break;
 
-    configureBindings();
+      default:
+        // Replayed robot, disable IO implementations
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {});
+       // flywheel = new Flywheel(new FlywheelIO() {});
+        break;
+    }
 
+    // Set up auto routines
+   /*  NamedCommands.registerCommand(
+        "Run Flywheel",
+        Commands.startEnd(
+                () -> flywheel.runVelocity(flywheelSpeedInput.get()), flywheel::stop, flywheel)
+            .withTimeout(5.0));*/
+
+    
+    configureButtonBindings();
   }
 
-  private void configureBindings() {
-
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
     drive.setDefaultCommand(
-      drive
-          .run(
-              () ->
-                  drive.acceptTeleopInput(
-                      -chassisDriver.getLeftY(),
-                      -chassisDriver.getLeftX(),
-                      -chassisDriver.getRightX(),
-                      false))
-          .withName("Drive Teleop Input"));
+        DriveCommands.joystickDrive(
+            drive,
+            () -> -controller.getLeftY(),
+            () -> -controller.getLeftX(),
+            () -> -controller.getRightX()));
+   
   }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return null;
   }
 }
-
