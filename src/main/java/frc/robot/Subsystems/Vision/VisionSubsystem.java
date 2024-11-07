e1c4c0c17c4c791af1f8b80ac6c1796bc5d2976b
SvenXD <Romanhernandezsosa1@gmail.com>
2024-11-06 20:22:06 -0600

    Added vision stuff, needs testing on real ll

--- src/main/java/frc/robot/Subsystems/Vision/VisionSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Vision/VisionSubsystem.java
@@ -4,7 +4,7 @@ import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.Util.LimelightHelpers;
--import frc.robot.Constants;
+import frc.robot.Constants;
 import frc.robot.Subsystems.Swerve.Drive;
 
 public class VisionSubsystem extends SubsystemBase {
